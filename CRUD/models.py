a8e231386b5cc2563f824c72ec6001ae91503fb9
Josguther1234 <josguther@mesoamericana.edu.gt>
2019-11-07 01:27:06 -0600

    se creo el primer modelo y se agrego la primera migracion de la tabla 'Animal'

--- CRUD/models.py
+++ CRUD/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+    nombre = models.CharField(max_length=70)
+    tipo = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    cantidad = models.IntegerField()
