a4eca1d1b1a994ffae8ce6e9dd4f9b29ca4fc8d2
Josguther1234 <josguther@mesoamericana.edu.gt>
2019-11-08 12:11:48 -0600

    Se agregaron las opciones de Encargado, animal y Horario al Dashboard

--- CRUD/views.py
+++ CRUD/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+
+def ListaEmpleado(request):
+    return render(request,'empleado/ListaEmpleado.html')
